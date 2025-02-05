adab7e20ca8167905ebd6e4598c3f5f1eb72fec9
mfaisal-kit <154584472+mfaisal-kit@users.noreply.github.com>
2025-02-05 16:38:57 +0500

    fresh copy upload of src

--- src/app/studio/[[...index]]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-import sanityConfig from "@/sanity/sanity.config";
-import { NextStudio } from "next-sanity/studio";
-
-export default function SanityStudio(){
-    return (<NextStudio config={sanityConfig}/>)
-}
