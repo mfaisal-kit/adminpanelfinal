965aa0577ea1588aa51708755e4a63e125707721
mfaisal-kit <154584472+mfaisal-kit@users.noreply.github.com>
2025-02-05 14:52:55 +0500

    Admin panel ready with add , edit , delete product and admin singin is working

---
+++ src/sanity/schema/index.ts
@@ -0,0 +1,10 @@
+import { mobileSchema } from "./mobile";
+import { productSchema } from "./products"
+import { adminUserSchema } from "./adminUser";
+
+
+export const schemaTypes = [
+                            mobileSchema, 
+                            productSchema,
+                            adminUserSchema
+                        ];
